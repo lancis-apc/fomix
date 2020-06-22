5f31d8b0196c7a6007099f734494675a21877f0d
aaron-ts <61706638+aaron-ts@users.noreply.github.com>
2020-06-22 16:39:47 -0500

    Creación tablas estados y municipios
    Estos archivos son para la creación y para la eliminación de las tablas más generales del sistema

---
+++ sgbs/db_banco_datos_yucatan.sql
@@ -0,0 +1,31 @@
+--Crea el schema
+CREATE SCHEMA IF NOT EXISTS development;
+
+--Esta es la sección "general"
+
+--Se crea la tabla para los estados
+CREATE TABLE development.estados (
+    clave_entidad CHAR(2) PRIMARY KEY,
+    entidad_federativa VARCHAR(32) NOT NULL
+);
+
+--Se crea la tabla para región
+CREATE TABLE development.regiones (
+    id_region SMALLINT PRIMARY KEY,
+    region VARCHAR(20) NOT NULL
+);
+
+--Se crea la tabla para los municipios
+CREATE TABLE development.municipios (
+    clave_municipio CHAR(3) PRIMARY KEY,
+    municipio VARCHAR(80) NOT NULL,
+    id_region SMALLINT REFERENCES development.regiones(id_region),
+    cve_ent CHAR(2) REFERENCES development.estados(clave_entidad)
+);
+
+--Se crea la tabla para las agebs
+CREATE TABLE development.agebs (
+    clave_ageb CHAR(4) PRIMARY KEY,
+    fol_ageb SMALLINT NOT NULL,
+    cve_mun CHAR REFERENCES development.municipios(clave_municipio)
+);
