fe916ba9283ca3ece9cc89ed64a95a6e6a539814
milocortes <hermilocg@domain.com>
2020-06-29 13:17:49 -0500

    Se agregan scripts del subsistema productivo

---
+++ codigos/productivo/denue_datos.R
@@ -0,0 +1,94 @@
+####### DENUE ###########
+### Limpiamos el área de trabajo ###
+rm(list=ls())
+
+### Cargamos las bibliotecas
+library(sqldf)
+library(stringi)
+library(reshape)
+
+### Cargamos las lista de csv por año
+
+lista_files<-as.character(unlist(read.table("/CARPETAS_TRABAJO/hcortes/denue/data/2015/files.txt",col.names = "file"),use.names = FALSE))
+lista_files<-paste("/CARPETAS_TRABAJO/hcortes/denue/data/2015/",lista_files,sep = "")
+
+### Usamos rbind para concatenar los datos
+denue<-data.frame()
+
+for (i in lista_files) {
+  datos<-read.csv(i)
+  denue<-rbind.data.frame(denue,datos)
+  print(i)
+}
+
+# Quitamos acentos y sustituimos los . por _ a los nombres de las columnas
+colnames(denue)<-chartr(".", "_",toupper(stri_trans_general(colnames(denue),"Latin-ASCII")))
+
+# Nos quedamos con los registros de Yucatán
+denue_yuc<-subset(denue,denue$CLAVE_ENTIDAD==31)
+denue_yuc_min<-denue_yuc[,c("CLAVE_MUNICIPIO","MUNICIPIO","CODIGO_DE_LA_CLASE_DE_ACTIVIDAD_SCIAN","DESCRIPCION_ESTRATO_PERSONAL_OCUPADO")]
+denue_yuc_min$rama<-substring(denue_yuc_min$CODIGO_DE_LA_CLASE_DE_ACTIVIDAD_SCIAN,1,4)
+
+consulta<-paste('SELECT CLAVE_MUNICIPIO,MUNICIPIO, DESCRIPCION_ESTRATO_PERSONAL_OCUPADO, rama, COUNT(rama)
+  AS Total FROM denue_yuc_min
+  GROUP BY CLAVE_MUNICIPIO,MUNICIPIO, DESCRIPCION_ESTRATO_PERSONAL_OCUPADO, rama')
+
+denue_yuc_rama<-sqldf(consulta)
+
+# Cambiamos el formato de CLAVE_MUNICIPIO, DESCRIPCION_ESTRATO_PERSONAL_OCUPADO y MUNICIPIO
+denue_yuc_rama$cve_mun<-denue_yuc_rama$CLAVE_MUNICIPIO
+denue_yuc_rama$DESCRIPCION_ESTRATO_PERSONAL_OCUPADO<-as.character(denue_yuc_rama$DESCRIPCION_ESTRATO_PERSONAL_OCUPADO)
+denue_yuc_rama$nom_mun<-as.character(denue_yuc_rama$MUNICIPIO)
+denue_yuc_rama$cve_ent<-"31"
+denue_yuc_rama$cvegeo<-paste("31",sprintf("%.3i",denue_yuc_rama$CLAVE_MUNICIPIO),sep="")
+
+# Agregamos el campo de region
+yuc_regiones<-read.csv("/CARPETAS_TRABAJO/hcortes/fomix/data/regiones_yuc.csv",sep=",")
+
+denue_yuc_rama<-merge(denue_yuc_rama,yuc_regiones,by="cve_mun",all.y=TRUE)
+
+denue_yuc_rama<-denue_yuc_rama[,c("cvegeo","cve_ent","cve_mun","nom_mun.y","region","DESCRIPCION_ESTRATO_PERSONAL_OCUPADO","rama","Total")]
+
+# Cambiamos las regiones
+region_func<-function(datos){
+  datos$region[datos$region=="IPoniente"]<-"I Poniente"
+  datos$region[datos$region=="IINoroeste"]<-"II Noroeste"
+  datos$region[datos$region=="IIICentro"]<-"III Centro"
+  datos$region[datos$region=="IVLitoral centro"]<-"IV Litoral centro"
+  datos$region[datos$region=="VNoreste"]<-"V Noreste"
+  datos$region[datos$region=="VIOriente"]<-"VI Oriente"
+  datos$region[datos$region=="VIISur"]<-"VII Sur"
+
+  return(datos$region)
+}
+
+denue_yuc_rama$region<-as.character(denue_yuc_rama$region)
+denue_yuc_rama$region<-region_func(denue_yuc_rama)
+# Exportamos como csv
+#write.csv(denue_yuc_rama,"/home/milo/Documentos/LANCIS/FOMIX/denue/output/denue_yuc_rama.csv",row.names = FALSE)
+
+# Exportamos como csv en formato wide
+library(tidyverse)
+
+denue_yuc_rama_wide<-cast(denue_yuc_rama,cvegeo+cve_ent+cve_mun+nom_mun.y+rama+region~DESCRIPCION_ESTRATO_PERSONAL_OCUPADO,sum)
+colnames(denue_yuc_rama_wide)<-chartr(" ", "_",stri_trans_general(colnames(denue_yuc_rama_wide),"Latin-ASCII"))
+
+denue_yuc_rama_wide$año<-2015
+denue_yuc_rama_wide<-denue_yuc_rama_wide[,c("cvegeo","cve_ent","cve_mun","nom_mun.y","region","año","rama", "0_a_5_personas","6_a_10_personas","11_a_30_personas","31_a_50_personas", "51_a_100_personas","101_a_250_personas","251_y_mas_personas")]
+
+colnames(denue_yuc_rama_wide)<-c("cvegeo","cve_ent","cve_mun","nom_mun","region","año","act_ec_cod", "0_a_5_personas","6_a_10_personas","11_a_30_personas","31_a_50_personas", "51_a_100_personas","101_a_250_personas","251_y_mas_personas")
+write.csv(denue_yuc_rama_wide,"/CARPETAS_TRABAJO/hcortes/denue/output/denue_yuc_rama_wide.csv",fileEncoding = "UTF-8",row.names = FALSE)
+
+### Agrupamos por subsector
+##denue_yuc_rama_wide<-read.csv("/home/milo/Documentos/LANCIS/FOMIX/denue/output/denue_yuc_rama_wide.csv")
+denue_yuc_subsector_wide<-denue_yuc_rama_wide
+denue_yuc_subsector_wide$act_ec_cod<-substring(denue_yuc_subsector_wide$act_ec_cod,1,2)
+
+library(dplyr)
+
+denue_yuc_subsector_wide<-denue_yuc_subsector_wide %>%
+  group_by(cvegeo,cve_ent,cve_mun,nom_mun,region,año,act_ec_cod) %>%
+  summarise_all(funs(sum))
+
+colnames(denue_yuc_subsector_wide)<-c("cvegeo","cve_ent","cve_mun","nom_mun","region","año","act_ec_cod", "0_a_5_personas","6_a_10_personas","11_a_30_personas","31_a_50_personas", "51_a_100_personas","101_a_250_personas","251_y_mas_personas")
+write.csv(denue_yuc_subsector_wide,"/CARPETAS_TRABAJO/hcortes/denue/output/denue_yuc_subsector_wide.csv",fileEncoding = "UTF-8",row.names = FALSE)
---
+++ codigos/productivo/yuc_regiones_subsector.R
@@ -0,0 +1,233 @@
+######## Script Censos Econ?micos 2009-2013 Yucat?n  ######
+### Limpiamos el ?rea de trabajo
+rm(list=ls())
+
+### Cargamos las bibliotecas
+library(sqldf)
+library(stringi)
+library(reshape)
+
+### Cargamos los datos
+
+yuc<-read.csv("/CARPETAS_TRABAJO/hcortes/fomix/data/yucatan_ce_09-14.csv")
+yuc_regiones<-read.csv("/CARPETAS_TRABAJO/hcortes/fomix/data/regiones_yuc.csv",sep=",")
+
+# Quitamos acentos y sustituimos los . por _ a los nombres de las columnas
+
+colnames(yuc)<-chartr(".", "_",toupper(stri_trans_general(colnames(yuc),"Latin-ASCII")))
+
+# Agrupamos a nivel subsector
+
+yuc$ACTIVIDAD_ECONOMICA<-substring(yuc$ACTIVIDAD_ECONOMICA,1,2)
+
+library(dplyr)
+
+yuc<-yuc %>%
+  group_by(ANO_CENSAL,ENTIDAD,MUNICIPIO,ACTIVIDAD_ECONOMICA) %>%
+  summarise_all(funs(sum))
+
+### Haremos una funci?n para obtener los porcentajes de participaci?n de cada
+#   Actividad Econ?mica en el total municipal de alguna de las variables censales.
+
+porc_tot_mun<-function(datos, variable){
+  sub_data<-datos[,c("ANO_CENSAL", "ENTIDAD", "MUNICIPIO", "ACTIVIDAD_ECONOMICA",variable)]
+  sub_data$id<- paste(sub_data$ANO_CENSAL,sub_data$ENTIDAD,sub_data$MUNICIPIO,sep="-")
+
+  consulta<-paste('SELECT ANO_CENSAL || "-"|| ENTIDAD ||"-"|| MUNICIPIO as id , SUM(',variable,')
+  AS Total FROM datos
+  GROUP BY ANO_CENSAL, ENTIDAD, MUNICIPIO')
+
+  sub_data_group<-sqldf(consulta)
+  sub_data_merge<-merge(sub_data,sub_data_group,by="id")
+
+  sub_data_merge$porcentaje<-sub_data_merge[,6]/sub_data_merge$Total
+
+  return(sub_data_merge)
+}
+
+### Hacemos un for para obtener el porcentaje de todas las variables
+
+yuc_porcentajes_act_ec_muni<-data.frame()
+
+lista_var<-colnames(yuc)[! colnames(yuc) %in% c("ANO_CENSAL","ENTIDAD","MUNICIPIO","ACTIVIDAD_ECONOMICA","Total")]
+
+for (i in lista_var) {
+  df_funct<-porc_tot_mun(yuc,i)
+  df_melt<-melt(df_funct, id=c("id", "ANO_CENSAL","ENTIDAD","MUNICIPIO","ACTIVIDAD_ECONOMICA","porcentaje"))
+  yuc_porcentajes_act_ec_muni<-rbind.data.frame(yuc_porcentajes_act_ec_muni,df_melt)
+  print(i)
+}
+
+#### Generamos un identificador cvgeo
+yuc_porcentajes_act_ec_muni$cvegeo<-paste(yuc_porcentajes_act_ec_muni$ENTIDAD,sprintf("%.3i",yuc_porcentajes_act_ec_muni$MUNICIPIO),sep = "")
+#### Agregamos la región a la que pertenece el municipio
+yuc_porcentajes_act_ec_muni$cve_mun<-yuc_porcentajes_act_ec_muni$MUNICIPIO
+yuc_porcentajes_act_ec_muni<-merge(yuc_porcentajes_act_ec_muni,yuc_regiones,by="cve_mun",all.y=TRUE)
+### Ordenamos las columnas del dataframe
+yuc_porcentajes_act_ec_muni$cve_ent<-yuc_porcentajes_act_ec_muni$ENTIDAD
+yuc_porcentajes_act_ec_muni<-subset(yuc_porcentajes_act_ec_muni,yuc_porcentajes_act_ec_muni$variable!="Total")
+
+yuc_porcentajes_act_ec_muni<-yuc_porcentajes_act_ec_muni[,c("cvegeo","cve_ent","cve_mun","nom_mun","region","ANO_CENSAL","ACTIVIDAD_ECONOMICA","variable","value","porcentaje")]
+yuc_porcentajes_act_ec_muni$porcentaje<-100*yuc_porcentajes_act_ec_muni$porcentaje
+
+## Guardamos los datos en formato long
+colnames(yuc_porcentajes_act_ec_muni)<-c("cvegeo","cve_ent","cve_mun","nom_mun","region","año","act_ec_cod_subsector","variable_censal","valor","porcentaje" )
+##write.csv(yuc_porcentajes_act_ec_muni,"/home/milo/Documentos/LANCIS/FOMIX/fomix/output/yuc_porcentajes_act_ec_subsector_muni.csv",fileEncoding = "UTF-8",row.names = FALSE)
+
+
+#### Generación de Matriz de ritmo de crecimiento de la inversión, personal ocupado, produccion fija bruta y unidades económicas por , municipio, por subsector y por subsector dentro de cada municipio
+library(tidyverse)
+
+#### Obtenemos la tasa de crecimiento por subsector
+
+yuc_porcentajes_act_ec_muni_subsector<-yuc_porcentajes_act_ec_muni[,c("act_ec_cod_subsector","año","variable_censal","valor")]
+
+consulta<-"SELECT act_ec_cod_subsector, año, variable_censal,SUM(valor) AS value
+            FROM yuc_porcentajes_act_ec_muni_subsector
+            GROUP BY act_ec_cod_subsector, año, variable_censal"
+
+yuc_porcentajes_act_ec_muni_subsector<-sqldf(consulta)
+
+panel_yuc_porcentajes_act_ec_muni_subsector<-cast(yuc_porcentajes_act_ec_muni_subsector,act_ec_cod_subsector+año~variable_censal)
+panel_yuc_porcentajes_act_ec_muni_subsector<-panel_yuc_porcentajes_act_ec_muni_subsector[,!colnames(panel_yuc_porcentajes_act_ec_muni_subsector) %in% c("Total")]
+
+## Generamos una función para obtener las tasas de crecimiento
+tasa_crecimiento_rama<-function(datos,variable_censal){
+  varname=paste0("tc_",variable_censal,sep = "")
+
+  datos %>%
+    arrange(act_ec_cod_subsector, año) %>%  # Ordenamos por municipio y por año
+    group_by(act_ec_cod_subsector) %>%  # Asignamos que dplyr dentro de cada municipio
+    mutate(sg.diff = !!as.name(variable_censal)  - lag(!!as.name(variable_censal)),
+           !!varname := (100*(sg.diff/lag(!!as.name(variable_censal))))
+    )
+}
+
+## Generamos una función para obtener las tasas de crecimiento media
+tasa_crecimiento_media_rama<-function(datos,variable_censal){
+  varname=paste0("tc_media_",variable_censal,sep = "")
+
+  datos %>%
+    arrange(act_ec_cod_subsector, año) %>%  # Ordenamos por municipio y por año
+    group_by(act_ec_cod_subsector) %>%  # Asignamos que dplyr dentro de cada municipio
+    mutate(sg.diff = (!!as.name(variable_censal)/lag(!!as.name(variable_censal)))^(1/5),
+           !!varname := (100*(sg.diff-1))
+    )
+}
+
+## Iteramos sobre la lista de variable_censals
+for (i in lista_var) {
+  panel_yuc_porcentajes_act_ec_muni_subsector<-tasa_crecimiento_rama(panel_yuc_porcentajes_act_ec_muni_subsector,i)
+  print(i)
+}
+
+for (i in lista_var) {
+  panel_yuc_porcentajes_act_ec_muni_subsector<-tasa_crecimiento_media_rama(panel_yuc_porcentajes_act_ec_muni_subsector,i)
+  print(i)
+}
+
+## Nos quedamos sólo con las tasas de crecimiento
+panel_yuc_porcentajes_act_ec_muni_subsector<-panel_yuc_porcentajes_act_ec_muni_subsector[! colnames(panel_yuc_porcentajes_act_ec_muni_subsector) %in% c(lista_var,"sg.diff")]
+## Cambiamos NA's por ceros
+panel_yuc_porcentajes_act_ec_muni_subsector[is.na(panel_yuc_porcentajes_act_ec_muni_subsector)]<-0
+
+## Nos quedamos sólo con el campo de inversión, personal ocupado, producción fija bruta y unidades económicas
+# inversion="tc_A211A_INVERSION_TOTAL__MILLONES_DE_PESOS_"
+# personal ocupado="tc_H001A_PERSONAL_OCUPADO_TOTAL"
+# produccion fija bruta="tc_A111A_PRODUCCION_BRUTA_TOTAL__MILLONES_DE_PESOS_"
+# unidades económicas="tc_UE_UNIDADES_ECONOMICAS"
+
+panel_yuc_porcentajes_act_ec_muni_subsector<-panel_yuc_porcentajes_act_ec_muni_subsector[,c("act_ec_cod_subsector","año","tc_media_A211A_INVERSION_TOTAL__MILLONES_DE_PESOS_","tc_media_H001A_PERSONAL_OCUPADO_TOTAL","tc_media_A111A_PRODUCCION_BRUTA_TOTAL__MILLONES_DE_PESOS_","tc_media_UE_UNIDADES_ECONOMICAS")]
+
+# Renombramos algunas columnas
+colnames(panel_yuc_porcentajes_act_ec_muni_subsector)<-c("act_ec_cod_subsector","año","tc_inv_total_mun","tc_per_ocupado_mun","tc_prod_brut_tot_mun","tc_ue_mun")
+
+# Hacemos un subset para no contar con los registros de 2004
+panel_yuc_porcentajes_act_ec_muni_subsector<-subset(panel_yuc_porcentajes_act_ec_muni_subsector,panel_yuc_porcentajes_act_ec_muni_subsector$año!=2004)
+
+## Guardamos el dataframe en formato csv
+colnames(panel_yuc_porcentajes_act_ec_muni_subsector)<-c("act_ec_cod_subsector","año","tc_inv_total_subsector","tc_per_ocupado_subsector","tc_prod_brut_tot_subsector","tc_ue_subsector")
+write.csv(panel_yuc_porcentajes_act_ec_muni_subsector,"/CARPETAS_TRABAJO/hcortes/fomix/output/bd_mat_tc_subsector.csv",fileEncoding = "UTF-8",row.names = FALSE)
+
+
+####### Obtenemos la tasa de crecimiento de cada subsector dentro del municipio
+
+yuc_porcentajes_act_ec_muni_wide<-cast(yuc_porcentajes_act_ec_muni[,! colnames(yuc_porcentajes_act_ec_muni) %in% c("porcentaje")],cvegeo+cve_ent+cve_mun+nom_mun+region+año+act_ec_cod_subsector~variable_censal,sum)
+yuc_porcentajes_act_ec_muni_wide<-yuc_porcentajes_act_ec_muni_wide[,! colnames(yuc_porcentajes_act_ec_muni_wide) %in% c("Total")]
+
+## Generamos una función para obtener las tasas de crecimiento
+tasa_crecimiento<-function(datos,variable){
+  varname=paste0("tc_",variable,sep = "")
+
+  datos %>%
+    arrange(cvegeo, año,act_ec_cod_subsector) %>%  # Ordenamos por municipio, año y rama
+    group_by(act_ec_cod_subsector) %>%  # Asignamos que dplyr dentro de cada municipio
+    mutate(sg.diff = !!as.name(variable)  - lag(!!as.name(variable)),
+           !!varname := (100*(sg.diff/lag(!!as.name(variable))))
+    )
+}
+
+## Generamos una función para obtener las tasas de crecimiento media
+tasa_crecimiento_media<-function(datos,variable){
+  varname=paste0("tc_media_",variable,sep = "")
+
+  datos %>%
+    arrange(cvegeo, año,act_ec_cod_subsector) %>%  # Ordenamos por municipio y por año
+    group_by(act_ec_cod_subsector) %>%  # Asignamos que dplyr dentro de cada municipio
+    mutate(anterior = (ifelse(lag(!!as.name(variable))==0, !!as.name(variable)*0.1,lag(!!as.name(variable) ))),
+           actual=(ifelse(!!as.name(variable)==0, lag(!!as.name(variable))*0.1, !!as.name(variable))),
+           sg.diff=(actual/anterior)^(1/5),
+           !!varname := (100*(sg.diff-1))
+    )
+}
+
+## Iteramos sobre la lista de variables
+
+for (i in lista_var) {
+  yuc_porcentajes_act_ec_muni_wide<-tasa_crecimiento(yuc_porcentajes_act_ec_muni_wide,i)
+  print(i)
+}
+
+for (i in lista_var) {
+  yuc_porcentajes_act_ec_muni_wide<-tasa_crecimiento_media(yuc_porcentajes_act_ec_muni_wide,i)
+  print(i)
+}
+
+## Nos quedamos sólo con las tasas de crecimiento
+yuc_porcentajes_act_ec_muni_wide<-yuc_porcentajes_act_ec_muni_wide[! colnames(yuc_porcentajes_act_ec_muni_wide) %in% c(lista_var,"sg.diff")]
+## Cambiamos NA's por ceros
+yuc_porcentajes_act_ec_muni_wide[is.na(yuc_porcentajes_act_ec_muni_wide)]<-0
+
+## Nos quedamos sólo con el campo de inversión, personal ocupado, producción fija bruta y unidades económicas
+# inversion="tc_A211A_INVERSION_TOTAL__MILLONES_DE_PESOS_"
+# personal ocupado="tc_H001A_PERSONAL_OCUPADO_TOTAL"
+# produccion fija bruta="tc_A111A_PRODUCCION_BRUTA_TOTAL__MILLONES_DE_PESOS_"
+# unidades económicas="tc_UE_UNIDADES_ECONOMICAS"
+
+yuc_porcentajes_act_ec_muni_wide<-yuc_porcentajes_act_ec_muni_wide[,c("cvegeo","cve_ent","cve_mun","nom_mun","region","año","act_ec_cod_subsector","tc_media_A211A_INVERSION_TOTAL__MILLONES_DE_PESOS_","tc_media_H001A_PERSONAL_OCUPADO_TOTAL","tc_media_A111A_PRODUCCION_BRUTA_TOTAL__MILLONES_DE_PESOS_","tc_media_UE_UNIDADES_ECONOMICAS")]
+
+# Renombramos algunas columnas
+colnames(yuc_porcentajes_act_ec_muni_wide)<-c("cvegeo","cve_ent","cve_mun","nom_mun","region","año","act_ec_cod_subsector","tc_inv_total_mun","tc_per_ocupado_mun","tc_prod_brut_tot_mun","tc_ue_mun")
+# Hacemos un subset para no contar con los registros de 2004
+yuc_porcentajes_act_ec_muni_wide<-subset(yuc_porcentajes_act_ec_muni_wide,yuc_porcentajes_act_ec_muni_wide$año!=2004)
+
+# Cambiamos las regiones
+region_func<-function(datos){
+  datos$region[datos$region=="IPoniente"]<-"I Poniente"
+  datos$region[datos$region=="IINoroeste"]<-"II Noroeste"
+  datos$region[datos$region=="IIICentro"]<-"III Centro"
+  datos$region[datos$region=="IVLitoral centro"]<-"IV Litoral centro"
+  datos$region[datos$region=="VNoreste"]<-"V Noreste"
+  datos$region[datos$region=="VIOriente"]<-"VI Oriente"
+  datos$region[datos$region=="VIISur"]<-"VII Sur"
+
+  return(datos$region)
+}
+
+
+yuc_porcentajes_act_ec_muni_wide$region<-as.character(yuc_porcentajes_act_ec_muni_wide$region)
+yuc_porcentajes_act_ec_muni_wide$region<-region_func(yuc_porcentajes_act_ec_muni_wide)
+
+## Guardamos el dataframe en formato csv
+colnames(yuc_porcentajes_act_ec_muni_wide)<-c("cvegeo","cve_ent","cve_mun","nom_mun","region", "año","act_ec_cod_subsector","tc_inv_total_mun","tc_per_ocupado_mun","tc_prod_brut_tot_mun","tc_ue_mun")
+write.csv(yuc_porcentajes_act_ec_muni_wide,"/CARPETAS_TRABAJO/hcortes/fomix/output/bd_mat_tc_muni_subsector.csv",fileEncoding = "UTF-8",row.names = FALSE)
