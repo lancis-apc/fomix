0424a4b0a3ca70ab9147337b3c048604ad5a1c21
aaron-ts <aaron.ts@outlook.com>
2020-08-28 18:14:47 -0500

    Actualización del componente general issue #19

---
+++ sgbs/general/datos_delete_banco_datos_yucatan.sql
@@ -0,0 +1,14 @@
+/*
+    ***********************************************************
+    Esta sección es la información que se usa de manera general
+    ***********************************************************
+*/
+
+--Borrando todos los datos de estados para correr el archivo sql
+DELETE FROM general.estados;
+
+--Borrando todos los datos de las regiones para correr el archivo sql
+DELETE FROM general.regiones;
+
+--Borrando todos los datos de municipios para correr el archivo sql
+DELETE FROM general.municipios;
